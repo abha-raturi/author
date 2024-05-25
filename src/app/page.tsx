160ddfea17873638887dd913d6014e29c42b1b63
sureshliqvid <suresh.bhatt@liqvid.com>
2024-05-25 22:03:48 +0530

    global and component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+import Login from "@/components/auth/Login";
 
 export default function Home() {
+ 
   return (
     <>
-    <h1>hello world!</h1>
-    <h2>hello world 2</h2>
+  
+       <Login/>
+    
     </>
   );
+
+  
 }
---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Card, Container } from "react-bootstrap";
+
+export default function Login() {
+    return (
+        <>
+            <Container>
+                <Card style={{ width: '18rem' }}>
+
+                    <Card.Body>
+                        <Card.Title>Login Here!</Card.Title>
+
+
+                    </Card.Body>
+                </Card>
+            </Container>
+        </>
+    );
+}
